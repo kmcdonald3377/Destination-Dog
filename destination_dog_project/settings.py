55e86fb08eabef8ece8e61c9e682b03bd11c524b
kmcdonald3377 <30431372+kmcdonald3377@users.noreply.github.com>
2018-02-28 00:10:00 +0000

    Update settings.py

--- destination_dog_project/settings.py
+++ destination_dog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g_&q4e7rf8@427q@y9fug272l$$302gqwix7nepeyko=x=wovg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['community.pythonanywhere.com']
 
