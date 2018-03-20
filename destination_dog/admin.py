91673f00c7ad0d9b5fc7dbc402c69c1d2f9d8260
Kayleigh Chisholm <2349784c@student.gla.ac.uk>
2018-03-20 13:38:27 +0000

    fixed article slug issue

--- destination_dog/admin.py
+++ destination_dog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from destination_dog.models import Article, UserProfile
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(UserProfile)
--- destination_dog/models.py
+++ destination_dog/models.py
@@ -3,15 +3,12 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
 class Article(models.Model):
-
     title = models.CharField(max_length=128, unique=True)
-
+    image = models.ImageField(upload_to='articles', blank=True, null=True) #For the top of the article page
     article = models.TextField()
     date = models.DateField()
     author = models.ForeignKey('UserProfile', related_name='article')
-    image = models.ImageField(upload_to='articles', blank=True, null=True) # To be shown at the top of the article
-
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
