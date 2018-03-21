c9b019f8b35af29243a695d91a6825de47dcd770
Stephanie Man <2286857m@student.gla.ac.uk>
2018-03-21 23:40:13 +0000

    update event form

--- destination_dog/forms.py
+++ destination_dog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from destination_dog.models import UserProfile, Article
+from destination_dog.models import UserProfile, Article, Event
 from datetime import date
 
 class UserForm(forms.ModelForm):
@@ -32,3 +32,16 @@ class AddArticleForm(forms.ModelForm):
             'article',
             'date',
         )
+
+class AddEventForm(forms.ModelForm):
+    name = forms.CharField(label="Event Name:", max_length=128,)
+    description = forms.charField(widget=forms.Textarea)
+    location = forms.CharField(label="Event Location:",)
+    date = forms.DateField(widget=forms.DateInput(format='%d-%m-%Y'))
+    time = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
+
+class Meta:
+        model = Event
+        fields = ('name','description', 'location', 'date', 'time')
+        widgets = {'description': forms.Textarea,
+        }
