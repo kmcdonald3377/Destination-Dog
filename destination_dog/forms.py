34c8d47219aa03fc3e43005a37fbea58dbbbe78b
Kayleigh Chisholm <2349784c@student.gla.ac.uk>
2018-03-21 18:34:23 +0000

    add article page - not working yet

--- destination_dog/forms.py
+++ destination_dog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from destination_dog.models import UserProfile
+from destination_dog.models import UserProfile, Article
+from datetime import date
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -13,3 +14,21 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
+
+class AddArticleForm(forms.ModelForm):
+
+    title = forms.CharField(max_length=128, help_text="Title:")
+    author = forms.CharField(max_length=128, help_text="Author:")
+    image = forms.ImageField(help_text="Header Image", required=False)
+    article = forms.CharField(widget=forms.Textarea, help_text="Article Content")
+    date = forms.DateField(widget=forms.HiddenInput(), initial=date.today())
+
+    class Meta:
+        model = Article
+        fields = (
+            'title',
+            'author',
+            'image',
+            'article',
+            'date',
+        )
