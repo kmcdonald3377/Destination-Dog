59bd36203762399c05824bfcfaf7f3bf7333956d
Stephanie Man <2286857m@student.gla.ac.uk>
2018-03-24 14:42:30 +0000

    updates

--- destination_dog_project/settings.py
+++ destination_dog_project/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
     'destination_dog',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES= [
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
