837d8c6ce0df2289661a01c482ad20dc20bb79a1
Kayleigh Chisholm <2349784c@student.gla.ac.uk>
2018-03-26 20:20:39 +0100

    changed

--- static/js/destinationdog-ajax.js
+++ static/js/destinationdog-ajax.js
@@ -1,7 +1,7 @@
 $('#likes').click(function(){
     var dotmid;
     dotmid = $(this).attr("data-dotmid");
-    $.get('/destinationdog/like/', {dotm_id : dotmid}, function(data){
+    $.get('/destinationdog/like/', dotmid, function(data){
         $('#like_count').html(data);
         $('#likes').hide();
     });
